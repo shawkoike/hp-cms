e990a7af171b4552f3ec22d70ef712554788486b
小池翔 <shaw@koikeshounoAir.mshome.net>
2018-01-27 22:24:15 +0900

    first commit

---
+++ demo20180102/src/main/java/com/example/demo/Demo20180102Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20180102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo20180102Application.class, args);
+	}
+}
