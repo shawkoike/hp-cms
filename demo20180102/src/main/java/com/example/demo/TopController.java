6b50a4c3767c840ccacf8fee6afd12cdbd0f8f97
keitabanno <keita.banno@gmail.com>
2018-01-27 23:59:31 +0900

    トップﾍﾟｰｼﾞ作成

---
+++ demo20180102/src/main/java/com/example/demo/TopController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TopController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+	  model.addAttribute("now", new Date().toString());
+	  return "top";
+	}
+}
+
